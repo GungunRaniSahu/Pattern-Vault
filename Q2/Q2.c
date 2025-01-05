3f72364fe28a7c032c79a1e7f717794e1db13d85
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-01-05 18:46:46 +0530

    Feat : Pyramid pattern in C

---
+++ Q2/Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void pyramidPattern(int rows) 
+{
+    for (int i = 1; i <= rows; i++) 
+    {
+        for (int j = 1; j <= rows - i; j++) 
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= 2 * i - 1; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() 
+{
+    int rows = 5; 
+    pyramidPattern(rows);
+    return 0;
+}
