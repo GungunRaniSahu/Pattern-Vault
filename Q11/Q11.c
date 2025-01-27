96ba952823d77d6632bfe13728fdad1549041b64
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-01-27 22:06:32 +0530

    Checkboard Pattern in C

---
+++ Q11/Q11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, j;
+
+    printf("Enter the size of the checkerboard: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {    
+            if ((i + j) % 2 == 0) 
+            {
+                printf("* ");
+            }
+            else 
+            {
+                printf("  ");
+            }
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
