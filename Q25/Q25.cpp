f6d3e22ed1d0198f89cf5bf2d0ad29b97bc01eec
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-03-01 19:58:29 +0530

    Create Q25.cpp

---
+++ Q25/Q25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i) {
+        for(int space = 1; space <= rows - i; ++space)
+            cout << " ";
+        for(int j = 1; j <= 2 * i - 1; ++j) {
+            if(i == rows || j == 1 || j == 2 * i - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
